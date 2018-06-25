5e6e72d006b60fc5c2654564f7101c41f3840fd1
ofir <ofirbux@gmail.com>
2018-06-25 22:16:15 +0300

    updated

--- script.js
+++ script.js
@@ -59,3 +59,35 @@ $(window).resize(function() {
  }
 
 });
+function isScrolledIntoView(elem) {
+	var docViewTop = $(window).scrollTop();
+	var docViewBottom = docViewTop + $(window).height();
+
+	var elemTop = $(elem).offset().top;
+	var elemBottom = elemTop + $(elem).height();
+
+	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function(){
+
+	if (isScrolledIntoView('.designimage') === true) {
+		
+
+
+		$('.designimage').addClass('animated fadeInUp');
+
+
+	}
+	if (isScrolledIntoView('.hiddendevelop') === true || 
+
+	isScrolledIntoView('.developimage') === true) {
+		
+
+		$('.hiddendevelop').addClass('animated fadeInUp');
+
+		$('.developimage').addClass('animated fadeInUp');
+
+	}
+
+});
